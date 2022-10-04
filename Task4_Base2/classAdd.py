8b02b179d1a070702e7448f6ee777d5316bfb849
Rafail Abdyushev <rafaelab@mail.ru>
2022-10-04 13:38:46 +0300

    Task4 convert to double base

---
+++ Task4_Base2/classAdd.py
@@ -0,0 +1,18 @@
+import os
+import random
+
+class InitSets:
+    def __init__(this):
+        clear = lambda: os.system("CLS")
+        clear()
+
+class ExecModule:
+    def __init__(this):
+        this.myNumber = random.randrange(233, 234)
+        print(f"Десятичное число: {this.myNumber}")
+        this.base2 = ""
+
+    def ConvertBase2(this):
+        while this.myNumber > 0:
+            this.base2 = str(this.myNumber % 2) + this.base2
+            this.myNumber = this.myNumber // 2
---
+++ Task4_Base2/convertToBase_2.py
@@ -0,0 +1,6 @@
+import classAdd
+
+initClear = classAdd.InitSets()
+work = classAdd.ExecModule()
+work.ConvertBase2()
+print(f"Двоичное число: {work.base2}")
